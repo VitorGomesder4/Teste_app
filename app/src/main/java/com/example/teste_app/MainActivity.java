81ac6b06586c4c1cc3ea9c21b0c50e587612d1fa
vitor gomes <vitor.araujo.derreteman4@gmail.com>
2025-08-25 22:19:30 -0300

    iniciando xml e java

--- app/src/main/java/com/example/teste_app/MainActivity.java
+++ app/src/main/java/com/example/teste_app/MainActivity.java
@@ -1,24 +1,40 @@
 package com.example.teste_app;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button meuBotao;
+    private EditText meuTexto;
+    private TextView meuNovoTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        meuBotao = findViewById(R.id.botao);
+        meuTexto = findViewById(R.id.texto);
+        meuNovoTexto = findViewById(R.id.novoTexto);
+        //Context ctx = getApplicationContext();
+
+        meuBotao.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //Toast torrada = Toast.makeText(ctx, meuTexto.getText(), Toast.LENGTH_LONG);
+                //torrada.show();
+
+                meuNovoTexto.setText(meuTexto.getText());
+            }
         });
     }
 }
